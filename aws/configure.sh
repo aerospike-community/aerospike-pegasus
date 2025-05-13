75c37dc9b9489298a850cac4335c08264cb66f57
behradbabaee <behrad@aerospike.com>
2025-05-13 16:50:39 +0100

    adding changes related to removal of old keycache

--- aws/configure.sh
+++ aws/configure.sh
@@ -5,21 +5,21 @@ AWS_EXPIRE="12h" # length of life of nodes prior to expiry; seconds, minutes, ho
 # Aerospike Config
 VER="8.0.0.4"
 CLUSTER_NAME="Demo"
-CLUSTER_NUMBER_OF_NODES="2"
-CLUSTER_INSTANCE_TYPE="i4i.4xlarge"
+CLUSTER_NUMBER_OF_NODES="5"
+CLUSTER_INSTANCE_TYPE="i7ie.3xlarge"
 
 # Namespace Config
 NAMESPACE_NAME="Test"
 NAMESPACE_DEFAULT_TTL="0"
 NAMESPACE_PRIMARY_INDEX_STORAGE_TYPE="MEMORY" #MEMORY, DISK
 NAMESPACE_SECONDARY_INDEX_STORAGE_TYPE="MEMORY" #MEMORY, DISK
-NAMESPACE_DATA_STORAGE_TYPE="MEMORY" #MEMORY, DISK
+NAMESPACE_DATA_STORAGE_TYPE="DISK" #MEMORY, DISK
 NAMESPACE_REPLICATION_FACTOR=2
 NAMESPACE_COMPRESSION="snappy" #none, lz4, snappy,i zstd
 
 # NVMe Config
-NUMBER_OF_PARTITION_ON_EACH_NVME="5"
-OVERPROVISIONING_PERCENTAGE=10
+NUMBER_OF_PARTITION_ON_EACH_NVME="6"
+OVERPROVISIONING_PERCENTAGE=15
 PRIMARY_INDEX_STORAGE_PARTITIONS="1"
 PARTITION_TREE_SPRIGS=65536
 SECONDARY_INDEX_STORAGE_PARTITIONS="3-4" # Set if the secondary indexes are on Disk.
@@ -31,22 +31,22 @@ GRAFANA_INSTANCE_TYPE="t3.xlarge"
 
 # Client Instance Config
 CLIENT_NAME="Perseus_${CLUSTER_NAME}"
-CLIENT_INSTANCE_TYPE="c6i.8xlarge" #Choose instances with more cpus, more than 32 GB of RAM, and no NVMe. C6a family are good choices.
+CLIENT_INSTANCE_TYPE="c6i.16xlarge" #Choose instances with more cpus, more than 32 GB of RAM, and no NVMe. C6a family are good choices.
 CLIENT_NUMBER_OF_NODES=1
 
 # Client Generic Workload Config
 TRUNCATE_SET=False
-RECORD_SIZE=1724 #Bytes. This test doesn't allow records smaller than 178 bytes!
+RECORD_SIZE=5120 #Bytes. This test doesn't allow records smaller than 178 bytes!
 BATCH_READ_SIZE=200
 BATCH_WRITE_SIZE=100
-READ_HIT_RATIO=.3
+READ_HIT_RATIO=.75
 
 # Client Caching Config
 KEY_CACHE_CAPACITY=1000000000 #The instance must have enough RAM to keep the key cache in memory. Each entry is 8 Bytes. 1 billion entries need 8 GB of Ram
 KEY_CACHE_SAVE_RATIO=1.0
 
 # Client Query Workload Config
-STRING_INDEX=False
+STRING_INDEX=True
 NUMERIC_INDEX=False
 GEO_SPATIAL_INDEX=False
 UDF_AFFREGATION=False
