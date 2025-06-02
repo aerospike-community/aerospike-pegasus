08ec7a1b4bca1e8c4e60ebfbf1335b285efa5f11
behradbabaee <behrad@aerospike.com>
2025-06-02 12:37:54 +0100

    config changes

--- aws/configure.sh
+++ aws/configure.sh
@@ -5,8 +5,8 @@ AWS_EXPIRE="12h" # length of life of nodes prior to expiry; seconds, minutes, ho
 # Aerospike Config
 VER="8.0.0.4"
 CLUSTER_NAME="Demo"
-CLUSTER_NUMBER_OF_NODES="2"
-CLUSTER_INSTANCE_TYPE="i7ie.3xlarge"
+CLUSTER_NUMBER_OF_NODES="4"
+CLUSTER_INSTANCE_TYPE="r6gd.4xlarge"
 
 # Namespace Config
 NAMESPACE_NAME="Test"
@@ -15,15 +15,15 @@ NAMESPACE_PRIMARY_INDEX_STORAGE_TYPE="MEMORY" #MEMORY, DISK
 NAMESPACE_SECONDARY_INDEX_STORAGE_TYPE="MEMORY" #MEMORY, DISK
 NAMESPACE_DATA_STORAGE_TYPE="DISK" #MEMORY, DISK
 NAMESPACE_REPLICATION_FACTOR=2
-NAMESPACE_COMPRESSION="snappy" #none, lz4, snappy,i zstd
+NAMESPACE_COMPRESSION="none" #none, lz4, snappy,i zstd
 
 # NVMe Config
-NUMBER_OF_PARTITION_ON_EACH_NVME="6"
-OVERPROVISIONING_PERCENTAGE=15
+NUMBER_OF_PARTITION_ON_EACH_NVME="10"
+OVERPROVISIONING_PERCENTAGE=0
 PRIMARY_INDEX_STORAGE_PARTITIONS="1"
 PARTITION_TREE_SPRIGS=65536
-SECONDARY_INDEX_STORAGE_PARTITIONS="3-4" # Set if the secondary indexes are on Disk.
-DATA_STORAGE_PARTITIONS="1-6" # Set if either the primary or secondary indexes are stored on Disk.
+SECONDARY_INDEX_STORAGE_PARTITIONS="1" # Set if the secondary indexes are on Disk.
+DATA_STORAGE_PARTITIONS="1-10" # Set if either the primary or secondary indexes are stored on Disk.
 
 # GRAFANA Config
 GRAFANA_NAME=${CLUSTER_NAME}"_GRAFANA"
@@ -32,30 +32,26 @@ GRAFANA_INSTANCE_TYPE="t3.xlarge"
 # Client Instance Config
 CLIENT_NAME="Perseus_${CLUSTER_NAME}"
 CLIENT_INSTANCE_TYPE="c6i.4xlarge" #Choose instances with more cpus, more than 32 GB of RAM, and no NVMe. C6a family are good choices.
-CLIENT_NUMBER_OF_NODES=2
+CLIENT_NUMBER_OF_NODES=1
 
 # Client Generic Workload Config
 TRUNCATE_SET=False
-RECORD_SIZE=1120 #Bytes. This test doesn't allow records smaller than 178 bytes!
+RECORD_SIZE=250 #Bytes. This test doesn't allow records smaller than 178 bytes!
 BATCH_READ_SIZE=200
-BATCH_WRITE_SIZE=100
-READ_HIT_RATIO=.75
-
-# Client Caching Config
-KEY_CACHE_CAPACITY=1000000000 #The instance must have enough RAM to keep the key cache in memory. Each entry is 8 Bytes. 1 billion entries need 8 GB of Ram
-KEY_CACHE_SAVE_RATIO=1.0
+BATCH_WRITE_SIZE=1000
+READ_HIT_RATIO=1
 
 # Client Query Workload Config
-STRING_INDEX=True
+STRING_INDEX=False
 NUMERIC_INDEX=False
 GEO_SPATIAL_INDEX=False
 UDF_AFFREGATION=False
-RANGE_QUERY=False
+RANGE_QUERY=True
 
 # Client Range Query Workload Config
-NORMAL_RANGE=5
+NORMAL_RANGE=10
 MAX_RANGE=100
-CHANCE_OF_MAX=.00001
+CHANCE_OF_MAX=.01
 
 # setup backend
 aerolab config backend -t aws -r ${AWS_REGION}
